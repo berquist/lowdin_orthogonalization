a285685d49a83581dbe4e4e6c10d45ff95e4f090
Eric Berquist <eric.berquist@gmail.com>
2017-11-08 14:07:50 -0500

    npl -> numpy.linalg

--- double_real.py
+++ double_real.py
@@ -2,7 +2,6 @@ from __future__ import print_function
 
 import numpy as np
 np.set_printoptions(precision=4, linewidth=200, suppress=True)
-import numpy.linalg as npl
 
 from common import read_arma_mat_ascii
 
@@ -11,9 +10,9 @@ S = read_arma_mat_ascii("S.dat")
 print("Overlap matrix")
 print(S)
 
-lam_s, l_s = npl.eigh(S)
+lam_s, l_s = np.linalg.eigh(S)
 lam_s = lam_s * np.eye(len(lam_s))
-lam_sqrt_inv = np.sqrt(npl.inv(lam_s))
+lam_sqrt_inv = np.sqrt(np.linalg.inv(lam_s))
 symm_orthog = np.dot(l_s, np.dot(lam_sqrt_inv, l_s.T))
 
 print("Symmetric orthogonalization matrix")
